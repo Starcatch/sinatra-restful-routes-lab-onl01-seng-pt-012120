d8013186013914d44c1e758f0e6221ca67f9e5fa
Timur Mishiev <timurmishiev@gmail.com>
2020-05-10 02:05:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,13 @@ class ApplicationController < Sinatra::Base
     @recipe.save
     redirect to "/recipes/#{@recipe.id}"
   end
+  
+    get '/recipes/:id' do
+      @recipe = Recipe.find_by_id(params[:id])
+      erb :show
+   end
+   
+   
 
 
 end
